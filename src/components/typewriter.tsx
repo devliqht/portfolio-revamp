238db3537916bfe164816a0fe7e1a6e75abb3871
Matt Cabarrubias <monapwc2020@gmail.com>
2025-05-30 10:50:13 +0800

    feat: added typewriter from previous portfolio

---
+++ src/components/typewriter.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useState, useEffect } from "react";
+
+interface TypewriterProps {
+  text: string;
+  delay: number;
+  infinite?: boolean;
+}
+
+const Typewriter: React.FC<TypewriterProps> = ({ text, delay, infinite }) => {
+  const [currentText, setCurrentText] = useState('');
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (currentIndex >= text.length) {
+      if (infinite) {
+        setTimeout(() => {
+          setCurrentIndex(0);
+          setCurrentText('');
+        }, delay);
+      }
+      return;
+    }
+
+    const timeout = window.setTimeout(() => {
+      setCurrentText(prevText => prevText + text[currentIndex]); 
+      setCurrentIndex(prevIndex => prevIndex + 1);
+    }, delay);
+
+    return () => clearTimeout(timeout);
+  }, [currentIndex, delay, infinite, text]);
+
+  return <span>{currentText}</span>;
+};
+
+export default Typewriter;
