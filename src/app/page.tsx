d2608feb3da4b95c79278e0479f23b6b2d19d01d
Matt Cabarrubias <monapwc2020@gmail.com>
2025-05-31 02:14:39 +0800

    feat: added projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,13 @@ import React from 'react';
 import Image from 'next/image';
 import { SiReact, SiVercel, SiPhp, SiLaravel, SiJavascript, SiTailwindcss } from 'react-icons/si';
 import Typewriter from '@/components/typewriter';
-import Header from '@/components/header';
+import ProjectsSection from '@/components/projects/page';
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <div className="relative min-h-screen flex text-white overflow-hidden wavy-gradient-bottom" id="landing">
         <div className="flex flex-col px-[5%] py-[3.7%] mt-8 z-10 relative h-fit transition" style={{transition: '1s all'}}>
-          <div 
-            className="absolute inset-0 z-0 opacity-60 pointer-events-none"
-          ></div>
           <h2 className="text-6xl font-dm-sans font-light tracking-tighter bg-gradient-to-r from-neutral-800 via-neutral-500 to-neutral-400 bg-clip-text text-transparent animate-float-in-bottom text-delay">Matt Cabarrubias</h2>
           <h1 className="text-[14vw] font-dm-sans text-black font-semibold tracking-tight hover:cursor-pointer hover:text-transparent hover:[-webkit-text-stroke:2px_black] -translate-y-[4vw]"><Typewriter text="devliqht" delay={150}/></h1>
           <h3 className="text-4xl font-baybayin text-black font-semibold tracking-tight pt-4 animate-float-in-top text-delay">devliqht</h3>
@@ -52,12 +48,14 @@ export default function Home() {
               src="/portraits/original.svg"
               alt="Matt Cabarrubias Portrait Original"
               fill
-              className="object-contain opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100"
+              className="object-contain opacity-0 transition-opacity duration-500 ease-in-out group-hover:opacity-100 drop-shadow-[0_0_12px_rgba(0,0,0,0.5)]"
               priority
             />
           </div>
         </div>
       </div>
+
+      <ProjectsSection />
     </main>
   );
 }
