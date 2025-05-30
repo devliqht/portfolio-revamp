8671910b38c74866e2abcb970b4c8a6b162454e7
Matt Cabarrubias <monapwc2020@gmail.com>
2025-05-30 10:52:25 +0800

    feat: add bg gradient, typewriter text and animated icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@
 import React from 'react';
 import Image from 'next/image';
 import { SiReact, SiVercel, SiPhp, SiLaravel, SiJavascript, SiTailwindcss } from 'react-icons/si';
+import Typewriter from '@/components/typewriter';
 
 export default function Home() {
   return (
     <main>
-      <div className="relative min-h-screen flex text-white overflow-hidden">
-        <div className="flex flex-col gap-4 px-[5%] py-[3.7%] mt-8 z-10 relative h-fit">
+      <div className="relative min-h-screen flex text-white overflow-hidden wavy-gradient-bottom" id="landing">
+        <div className="flex flex-col gap-4 px-[5%] py-[3.7%] mt-8 z-10 relative h-fit transition" style={{transition: '1s all'}}>
           <div 
             className="absolute inset-0 z-0 opacity-60 pointer-events-none"
             style={{
@@ -18,37 +19,37 @@ export default function Home() {
             }}
           ></div>
           <h2 className="text-6xl font-dm-sans text-black font-light tracking-tighter">Matt Cabarrubias</h2>
-          <h1 className="text-8xl font-dm-sans text-black font-semibold tracking-tight">devliqht</h1>
+          <h1 className="text-8xl font-dm-sans text-black font-semibold tracking-tight"><Typewriter text="devliqht" delay={150}/></h1>
           <h3 className="text-4xl font-baybayin text-black font-semibold tracking-tight pt-4">devliqht</h3>
         </div>
         <div className="absolute right-0 top-8/12 scale-110 -translate-y-1/2 w-[400px] h-[100%] group">
           <div className="absolute inset-0 -z-10">
-            <div className="absolute top-[48%] right-8 hover:scale-110">
+            <div className="absolute top-[48%] right-8 hover:scale-110 animate-float-in-right animate-delay-100">
               <SiReact className="w-32 h-32 text-black opacity-100" />
             </div>
-            <div className="absolute top-1/8 -left-0 -rotate-24">
+            <div className="absolute top-1/8 -left-0 -rotate-24 animate-float-in-left animate-delay-100">
               <SiVercel className="w-38 h-38 text-black opacity-100" />
             </div>
-            <div className="absolute bottom-1/5 right-8">
+            <div className="absolute bottom-1/5 right-8 animate-float-in-bottom animate-delay-100">
               <SiPhp className="w-38 h-38 text-black opacity-100" />
             </div>
-            <div className="absolute bottom-1/3 -right-12 animate-float">
-              <SiLaravel className="w-8 h-8 text-[#FF2D20] opacity-80" />
+            <div className="absolute top-1/7 right-9 rotate-11 animate-float-in-top animate-delay-100">
+              <SiLaravel className="w-42 h-42 text-black opacity-100" />
             </div>
-            <div className="absolute top-1/2 -left-4 -rotate-16">
+            <div className="absolute top-[60%] -left-4 -rotate-16 animate-float-in-bottom animate-delay-100">
               <SiJavascript className="w-28 h-28 text-black opacity-100" />
             </div>
-            <div className="absolute top-1/2 -right-16 animate-float">
-              <SiTailwindcss className="w-8 h-8 text-[#06B6D4] opacity-80" />
+            <div className="absolute top-1/3 -left-0 animate-float-in-left animate-delay-100">
+              <SiTailwindcss className="w-32 h-32 text-black opacity-100" />
             </div>
           </div>
           
-          <div className="relative w-full h-full transition-all duration-500 ease-in-out group-hover:scale-100">
+          <div className="relative w-full h-full transition-all duration-500 ease-in-out group-hover:scale-105">
             <Image
               src="/portraits/halftone.svg"
               alt="Matt Cabarrubias Portrait Halftone"
               fill
-              className="object-contain transition-opacity duration-500 ease-in-out group-hover:opacity-0 drop-shadow-[0_0_12px_rgba(0,0,0,0.5)]"
+              className="object-contain transition-opacity duration-500 ease-in-out group-hover:opacity-0"
               priority
             />
             <Image
